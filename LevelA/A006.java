fef464ebf389f215c66c4c0e20e69f68cee2ed19
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-01-01 22:02:55 +0900

    A006 solve

---
+++ LevelA/A006.java
@@ -0,0 +1,27 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int pNum=0;
+        int yNum=0;
+        char[] charArray = new char[s.length()];
+        for(int i=0; i<s.length(); i++){
+            charArray[i] =  s.charAt(i);
+        }
+        
+        
+        for(char cha : charArray){
+            if(cha=='p' || cha=='P'){
+                pNum++;
+            } else if (cha=='y' || cha=='Y'){
+                yNum++;
+            }
+        }
+        if(pNum==yNum){
+            answer=true;
+        } else {
+            answer=false;
+        }
+        
+        return answer;
+    }
+}
